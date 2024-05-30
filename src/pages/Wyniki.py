e76a9f38447a47545b87c87751e8464bae7d0b68
Agata Szydełko <agata.szydelko@outlook.com>
2024-05-30 19:05:45 +0200

    change page Wyniki

--- src/pages/Wyniki.py
+++ src/pages/Wyniki.py
@@ -1,86 +1,80 @@
-import streamlit as st
-
 import os
 import pandas as pd
 import streamlit as st
 import seaborn as sns
 import matplotlib.pyplot as plt
+from src.DataManager import data_manager
 
 class Results:
     def __init__(self):
-        self.file_path = "src/resources/scores.csv"
-        self.score_float = None
+        self.db_manager = data_manager()
+        self.results_placeholder = st.empty()
 
-        if 'scores' not in st.session_state:
-            if os.path.exists(self.file_path):
-                st.session_state.scores = self.load_scores()
-            else:
-                st.session_state.scores = []
-
-    def load_scores(self):
-        scores_read = pd.read_csv(self.file_path)
-        return scores_read['Wyniki'].tolist()
-
-    def save_scores(self):
-        scores_save = pd.DataFrame(st.session_state.scores, columns=["Wyniki"])
-        scores_save.to_csv(self.file_path, index=False)
-
-    def clear_scores(self):
-        st.session_state.scores = []
-        empty_scores = pd.DataFrame(columns=["Wyniki"])
-        empty_scores.to_csv(self.file_path, index=False)
-
-    def print(self):
+    def print_summary(self):
         st.title("Wyniki")
         st.subheader("Część 1 materiału")
 
-        # wprowadzanie i zatwierdzanie wyniku (kiedys czytanie z bazy)
-        score = st.text_input("Podaj wynik (0-100):")
-        if st.button("Dodaj wynik"):
-            try:
-                self.score_float = float(score)
-                if  0 <= self.score_float <= 100:
-                    st.session_state.scores.append(self.score_float)
-                    st.session_state.scores = sorted(st.session_state.scores, reverse=True)[:10]
+        results_df = self.db_manager.show_results()
+        if not results_df.empty:
+            results_df = self.calculate_percentage(results_df)
 
-                    self.save_scores()
+            # Display top 10 scores
+            top_scores_df = self.get_top_scores(results_df, n=10)
 
-                else:
-                    st.error("Wprowadź poprawny wynik od 0 do 100.")
-            except ValueError:
-                st.error("Wprowadź poprawny wynik.")
+            if not top_scores_df.empty:
+                st.table(top_scores_df[['UserID', 'Percentage']])
+            else:
+                st.write("Brak najlepszych wyników do wyświetlenia.")
 
-        # wyswietlanie 10 najlepszych wynikow w tabeli i na wykresie
-        if st.session_state.scores:
-            top_scores = sorted(st.session_state.scores, reverse=True)[:10]
-            top_scores_df = pd.DataFrame(top_scores, columns=["Wynik [%]"])
-            top_scores_df.index = top_scores_df.index + 1
-            st.subheader("Najlepsze wyniki")
-            st.table(top_scores_df)
+            if st.button("Wyczyść wyniki"):
+                # self.clear_overall_results()
+                st.success("Wyniki zostały wyczyszczone.")
         else:
-            st.write("Tabela jest pusta.")
+            st.write("Brak wyników w bazie danych.")
 
-        self.plot_histogram(self.score_float)
+        self.display_session_results()
+    def display_session_results(self):
+        if 'results' in st.session_state and st.session_state['results']:
+            st.subheader("Wyniki z bieżącej sesji")
+            for i, result in enumerate(st.session_state['results']):
+                percentage = (result['score'] / result['total']) * 100
+                st.write(f"Wynik ostatniego podejścia: {result['score']} / {result['total']} ({percentage:.2f}%)")
+        else:
+            st.write("Brak wyników w bieżącej sesji.")
 
-        # czyszczenie tabeli, wykresu, csv
-        if st.button("Wyczyść tabelę"):
-            self.clear_scores()
+    def calculate_percentage(self, df):
+        df['Percentage'] = (df[['Result1', 'Result2', 'Result3', 'Result4', 'Result5']].sum(axis=1) / 5) * 100
+        return df
 
-        st.subheader("Część 2 materiału")
+    def get_top_scores(self, results_df, n=10):
+        if not results_df.empty:
+            top_scores_df = results_df.sort_values(by='Percentage', ascending=False).head(n)
+            top_scores_df.reset_index(drop=True, inplace=True)
+            return top_scores_df[['UserID', 'Percentage']]
+        else:
+            return pd.DataFrame()
 
-    def plot_histogram(self, highlight_score=None):
-        sns.set(style="whitegrid")
-        fig, ax = plt.subplots(figsize=(8, 6))
-        sns.histplot(st.session_state.scores, bins=10, kde=True, ax=ax)
-        plt.xlabel("Wynik")
-        plt.ylabel("Liczba")
-        plt.title("Wyniki")
-        if highlight_score is not None:
-            plt.axvline(highlight_score, color='r', linestyle='--', label=f'Nowy wynik: {highlight_score}')
-            plt.legend()
-        st.pyplot(fig)
+    def print_histogram(self):
+        results_df = self.db_manager.show_results()
+        if not results_df.empty:
+            results_df = self.calculate_percentage(results_df)
+            scores = results_df['Percentage'].tolist()
+            sns.set(style="whitegrid")
+            fig, ax = plt.subplots(figsize=(8, 6))
+            sns.histplot(scores, bins=10, kde=True, ax=ax)
+            plt.xlabel("Wynik")
+            plt.ylabel("Liczba")
+            plt.title("Wyniki z wszystkich podejść")
+            st.pyplot(fig)
+        else:
+            st.write("Brak wyników do wyświetlenia.")
 
-
+    # def clear_overall_results(self):
+    #     # Perform database operation to clear overall_result data
+    #     with self.db_manager.conn.cursor() as cur:
+    #         cur.execute("DELETE FROM riaoAttempts")
+    #         self.db_manager.conn.commit()
 
 results = Results()
-results.print()
+results.print_summary()
+results.print_histogram()
